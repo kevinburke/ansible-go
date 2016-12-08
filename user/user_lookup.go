7f4a4667fbb1de0fa0c0c96ead0a5669509925ca
Kevin Burke <kev@inburke.com>
2016-12-08 07:51:33 -0800

    flush out findUserName

--- user/user_lookup.go
+++ user/user_lookup.go
@@ -1,8 +1,11 @@
 package user
 
 import (
+	"bufio"
 	"io"
 	"os"
+	"os/user"
+	"strings"
 )
 
 const passwdFile = "/etc/passwd"
@@ -17,5 +20,23 @@ func lookupUser(username string) (bool, error) {
 }
 
 func findUserName(name string, r io.Reader) (bool, error) {
-	return false, nil
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		text := trimSpace(removeComment(scanner.Text()))
+		if len(text) == 0 {
+			continue
+		}
+		// wheel:*:0:root
+		parts := strings.SplitN(text, ":", 4)
+		if len(parts) < 4 {
+			continue
+		}
+		if parts[0] == name && parts[2] != "" {
+			return &user.Group{Name: parts[0], Gid: parts[2]}, nil
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return nil, user.UnknownGroupError(name)
 }
